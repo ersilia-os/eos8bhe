ae35d69f50176815bd2a82b0227aa8a41b416152
Ini-Abasi Bernard <55382743+Inyrkz@users.noreply.github.com>
2024-02-29 15:02:48 +0100

    Update main.py
    Update output format

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -20,14 +20,19 @@ safe_list = [designer.smiles_to_safe(smi) for smi in smiles_list]
 # run model
 outputs = designer.run_model(safe_list)
 
-#check input and output have the same lenght
+#check input and output have the same length
 input_len = len(smiles_list)
 output_len = len(outputs)
 assert input_len == output_len
 
 # write output in a .csv file
+N_COLS = 1000
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"])  # header
+    h = ["smi_{}".format(i) for i in range(N_COLS)]
+    writer.writerow(h)
     for o in outputs:
-        writer.writerow([o])
+        o = o[:N_COLS]
+        if len(o) < N_COLS:
+            o = o + [None] * (N_COLS - len(o))
+        writer.writerow(o)
